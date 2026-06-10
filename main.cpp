bcc509eebd749e566ce4a7a0f2f111df2ad8bc7b
Ammar Bin Yasir <ammarbinyasir2007@gmail.com>
2026-06-11 01:36:32 +0500

    added cross platform compability

--- main.cpp
+++ main.cpp
@@ -2,12 +2,36 @@
 #include "core/ConsoleUI.h"
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h>
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#endif
+#endif
+
 using namespace std;
 
 Date currentSystemDate(9, 6, 2026);
 
 int main() 
 {
+    #ifdef _WIN32
+    // Force UTF-8 Output encoding on Windows consoles to render box-drawing characters
+    SetConsoleOutputCP(CP_UTF8);
+
+    // Enable Virtual Terminal Processing for ANSI colors/screen clears
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut != INVALID_HANDLE_VALUE) 
+    {
+        DWORD dwMode = 0;
+        if (GetConsoleMode(hOut, &dwMode)) 
+        {
+            dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            SetConsoleMode(hOut, dwMode);
+        }
+    }
+    #endif
+
     // Enable ANSI console formatting
     ConsoleUI::clearScreen();
     
